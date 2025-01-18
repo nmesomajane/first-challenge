a8e591461a32c6b9d2c62636e0d7ec01c2eaea6e
nmesomajane <nnopujane@gmail.com>
2025-01-18 19:01:20 +0100

    corrected error

--- src/App.js
+++ src/App.js
@@ -1,143 +1,163 @@
-import  React,{ useEffect, useState} from 'react';
-
-// import logo from './logo.svg';
-import './App.css';
-
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 const ToDo = () => {
-  const [catchVal, setCatchval] = useState('')
-  const [tasklist, setTaskList] =useState(()=>{
-    const todoItem = localStorage.getItem("Todos")
-    return todoItem ? JSON.parse(todoItem) :[]
-  })
-  const[isEdit,setIsEdit]=useState(false)
-  const [editTaskindex, setEditTaskIndex]= useState(null)
-  const [filter,setFilter] =useState('all')
+  const [catchVal, setCatchval] = useState("");
+  const [tasklist, setTaskList] = useState([]);
+  const [filteredList, setFilteredList] = useState([]);
+  const [itemLength, setItemLength] = useState(0);
+  const [isEdit, setIsEdit] = useState(false);
+  const [editTaskId, setEditTaskId] = useState(null);
+  const [filter, setFilter] = useState("all");
 
-  useEffect(()=> {
-    localStorage.setItem('Todos',JSON.stringify(tasklist))
-  },[tasklist])
+  useEffect(() => {
+    const todoItem = localStorage.getItem("Todos");
+    if (todoItem) {
+      const parsed = JSON.parse(todoItem);
+      setTaskList(parsed);
+    }
+  }, []);
 
-  const addTodos = (e) =>{
-    e.preventDefault()
-      
-    const newTask ={
-      id: Date.now() +"" ,
-      task: catchVal
+  const addTodos = (e) => {
+    e.preventDefault();
+
+    if (catchVal.trim() === "") {
+      alert("Add a task");
+      return;
     }
 
-    setTaskList([newTask, ...tasklist])
+    if (isEdit) {
+      const updatedList = tasklist.map((item) => {
+        return item.id === editTaskId ? { ...item, task: catchVal } : item;
+      });
 
-    setCatchval("")
-
-    if(catchVal.trim() === ""){
-      alert("Add a task")
-      return
+      setTaskList(updatedList);
+      setIsEdit(false);
+      localStorage.setItem("Todos", JSON.stringify(updatedList));
+    } else {
+      const newTask = {
+        id: Date.now() + "",
+        task: catchVal,
+        status: "pending",
+      };
+      const newList = [newTask, ...tasklist];
+      setTaskList(newList);
+      localStorage.setItem("Todos", JSON.stringify(newList));
     }
 
-    if(isEdit){
-      const updatedList = tasklist.map((item,i) =>{
-         return i === editTaskindex ? { ...item, task:catchVal} :item
-      })
+    setCatchval("");
+  };
 
-      setTaskList(updatedList)
-      setIsEdit(false)
+  const deleteTask = (item) => {
+    const confirmDelete = window.confirm(
+      `Are you sure you want to delete this ${item.task} permanently`
+    );
+    if (confirmDelete) {
+      const updatedlist = tasklist.filter((task) => task.id !== item.id);
+      setTaskList(updatedlist);
     }
-    else{
-      const newTask ={
-      id: Date.now() + '',
-      task: catchVal,
-      status: 'pending'
+  };
+
+  const editTask = (id) => {
+    setIsEdit(true);
+    const listToEdit = tasklist.find((task) => task.id === id);
+    if (listToEdit) {
+      setCatchval(listToEdit?.task);
+      setEditTaskId(id);
     }
+  };
+  const toggleStatus = (id) => {
+    const updatedList = tasklist.map((item) =>
+      item.id === id
+        ? {
+            ...item,
+            status: item.status === "pending" ? "complete" : "pending",
+          }
+        : item
+    );
+    setTaskList(updatedList);
+    localStorage.setItem("Todos", JSON.stringify(updatedList));
+  };
 
-    }
+  useEffect(() => {
+    const filteredList = tasklist.filter((item) => {
+      if (filter === "complete") return item.status === "complete";
+      if (filter === "pending") return item.status === "pending";
+      return true;
+    });
+    setFilteredList(filteredList);
+    setItemLength(filteredList.length);
+  }, [filter, tasklist]);
 
-    
-
-    setTaskList([newTask,...tasklist]) 
-
-    setCatchval('')
-  }
-  const deleteTask = (item)=>{
-    const confirmDelete =window.confirm('Are you sure you want to delete this ${item.task} permanently') 
-    if(confirmDelete){
-      const updatedlist =FileList.filter((task) =>task.id !== item.id)
-      setTaskList(updatedlist)
-    }
-  }
-
-  const editTask =(index) =>{
-      setIsEdit(true);
-      setCatchval(tasklist[index].task);
-      setEditTaskIndex(index);
-
-  }
-  const toggleStatus =(id) => {
-      const updatedList = tasklist.map((item) =>
-        item.id === id ?{...item,status: item.status === 'pending' ? "complete" :'pending'} : item)
-        setTaskList(updatedList)
-  }
-  const filteredList = tasklist.filter((item)=>{
-    if(filter=== 'complete') return item.status === 'complete'
-    if(filter=== 'pending') return item.status === 'pending'
-    return true
-  })
-
-  return(
-    <div className='container'>
+  return (
+    <div className="container">
       <form onSubmit={addTodos}>
-        <input type='text'
-        value={catchVal}
-        onChange={(e)=>{
-           setCatchval(e.target.value)
-           }} ></input>
-        <button type='submit'> {isEdit ? 'update Tasks' :'Add task'}</button>
+        <input
+          type="text"
+          value={catchVal}
+          onChange={(e) => {
+            setCatchval(e.target.value);
+          }}
+        ></input>
+        <button type="submit"> {isEdit ? "update Tasks" : "Add task"}</button>
       </form>
 
-      <div className='compen'> 
-          <button onclick={()=>setFilter("all")}>All Task</button>
-          <button onclick={()=>setFilter("pending")}>Pending</button>
-          <button onclick={()=>setFilter("complete")}>Completed</button>
+      <div className="compen">
+        <button onClick={() => setFilter("all")}>All Task</button>
+        <button onClick={() => setFilter("pending")}>Pending</button>
+        <button onClick={() => setFilter("complete")}>Completed</button>
       </div>
 
-      <div className='todocontainer'>
-
-           {filteredList.lenght > 0? (
-            filteredList.map((item,index) =>(
-              <div className='tasks' key={item.id}>
-            <div>
-              <input  
-              type='checkbox' 
-              checked = {item.status === 'completed'}
-              onChange={()=> toggleStatus(item.id)} ></input>
-            </div>
-            <div className='todo'>
-              <span style={{textdecoration: item.status === "complete" ? 'line-trough': 'none'}}> {item.span}</span>
-              <div className='btn'>
-                <button className='edit' onClick={ (e)=>editTask (index)}>Edit</button>
-                <button className='delete' onclick ={(e)=>deleteTask(item.id)}>Delete</button>
+      <div className="todocontainer">
+        {filteredList.length ? (
+          filteredList.map((item, index) => (
+            <div className="tasks" key={item.id}>
+              <div>
+                <input
+                  type="checkbox"
+                  checked={item.status === "complete"}
+                  onChange={() => toggleStatus(item.id)}
+                />
+              </div>
+              <div className="todo">
+                <span
+                  style={{
+                    textdecoration:
+                      item.status === "complete" ? "line-trough" : "none",
+                  }}
+                >
+                  {item.task}
+                </span>
+                <div className="btn">
+                  <button className="edit" onClick={() => editTask(item.id)}>
+                    Edit
+                  </button>
+                  <button
+                    className="delete"
+                    onClick={() => deleteTask(item.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-            ))
-           ) : (<p>
-                 {filter==='complete' && "there is no complete tasks"}
-                 {filter==='pending' && "there is no pending tasks"}
-                 {filter==='all' && "No task available here"}
-           </p>)}
-          
+          ))
+        ) : (
+          <p>
+            {filter === "complete" && "there is no complete tasks"}
+            {filter === "pending" && "there is no pending tasks"}
+            {filter === "all" && "No task available here"}
+          </p>
+        )}
       </div>
 
-      <div className='counter'>
-        {filter === 'all' && <span>All Task = {filteredList.lenght} </span>}
-        {filter === 'complete' && <span>Complete task= {filteredList.lenght} </span>}
-        {filter === 'pending' && <span>pending Task = {filteredList.lenght} </span>}
+      <div className="counter">
+        {filter === "all" && <span>All Task = {itemLength} </span>}
+        {filter === "complete" && <span>Complete task= {itemLength} </span>}
+        {filter === "pending" && <span>pending Task = {itemLength} </span>}
       </div>
     </div>
-
-    
-  )
-}
+  );
+};
 
 export default ToDo;
